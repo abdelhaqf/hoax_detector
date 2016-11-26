0d1cc0939590e4108412173b45bb2133fb521e93
abiyogaaron <abiyogaaron@gmail.com>
2016-11-26 17:28:37 +0700

    topic

--- CI/application/controllers/Home.php
+++ CI/application/controllers/Home.php
@@ -27,6 +27,46 @@ class Home extends CI_Controller {
 		$data['footer'] = $this->load->view('Template/footer',NULL,TRUE);
 		$data['header'] = $this->load->view('Template/header',NULL,TRUE);
 
+		//AMBIL DARI EXPLORE 
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL,"https://hack.kurio.co.id/v1/explore");
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		$headers = [
+    		'Accept:application/json',
+			'X-Kurio-Client-ID:99',
+			'X-Kurio-Client-Secret:S3VyaW9IYWNrYXRvbjIw',
+			'X-OS:windows',
+			'X-App-Version:1.0',
+			'Content-Type:application/json',
+			'Authorization:Bearer M1G6qRcD372I8aMfKzi2TLaBqNex6onKTXi6jIfM'
+		];
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		$json_string = curl_exec ($curl);
+		$array_topic = json_decode($json_string,true);
+
+		$i = 0;
+
+		//PARSING DARI JSON YANG UDAH DIDECODE
+		foreach($array_topic as $data)
+		{
+			foreach($data as $values)
+			{
+				foreach($values as $value)
+				{
+					$arrTopicName[$i] = $value['name'];
+					$arrTopicId[$i] = $value['id'];
+					$i++;
+				}
+			}
+		}
+		
+		$data['TopicName'] = $arrTopicName;
+		$data['TopicId'] = $arrTopicId;
+		
+
 		$this->load->view('Page/home',$data);
 	}
 }
